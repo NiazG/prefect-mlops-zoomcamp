1534faa90d43b719820c9975f435934216d8ce73
Niiaz Ildusovich Gainutdinov <ng1987@mail.ru>
2023-06-14 12:06:16 +0700

    week3

--- 3.5/orchestrate_s3.py
+++ 3.5/orchestrate_s3.py
@@ -9,7 +9,6 @@ from sklearn.metrics import mean_squared_error
 import mlflow
 import xgboost as xgb
 from prefect import flow, task
-from prefect_aws import S3Bucket
 from prefect.artifacts import create_markdown_artifact
 from datetime import date
 
@@ -132,8 +131,8 @@ def train_best_model(
 
 @flow
 def main_flow_s3(
-    train_path: str = "./data/green_tripdata_2021-01.parquet",
-    val_path: str = "./data/green_tripdata_2021-02.parquet",
+    train_path: str = "./data/green_tripdata_2023-02.parquet",
+    val_path: str = "./data/green_tripdata_2023-03.parquet",
 ) -> None:
     """The main training pipeline"""
 
@@ -142,8 +141,8 @@ def main_flow_s3(
     mlflow.set_experiment("nyc-taxi-experiment")
 
     # Load
-    s3_bucket_block = S3Bucket.load("s3-bucket-block")
-    s3_bucket_block.download_folder_to_path(from_folder="data", to_folder="data")
+    #s3_bucket_block = S3Bucket.load("s3-bucket-block")
+    #s3_bucket_block.download_folder_to_path(from_folder="data", to_folder="data")
 
     df_train = read_data(train_path)
     df_val = read_data(val_path)
